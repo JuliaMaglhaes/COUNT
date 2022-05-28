b86380fb271df882b324f8a815345c079b81f718
Julia Magalhães <juliamagalhaes28@outlook.com>
2022-05-27 22:44:59 -0300

    make it better

--- counteyeapi/views.py
+++ counteyeapi/views.py
@@ -1,4 +1,5 @@
 from threading import Thread
+from core.settings import BASE_DIR
 from rest_framework import generics
 from rest_framework.permissions import SAFE_METHODS,AllowAny,  IsAuthenticated, DjangoModelPermissions, BasePermission
 from .serializers import CountSerializer
@@ -101,8 +102,10 @@ COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
 
 class_names = [c.strip() for c in open('./counteyeapi/services/detection/names.names').readlines()]
 
-modelWeightsPath = os.path.abspath('yolov3_training_last.weights')
-modelConfigurationPath = os.path.abspath('yolov3_testing.cfg')
+modelWeightsPath = os.path.join(BASE_DIR, 'yolov3_training_last.weights')
+modelConfigurationPath = os.path.join(BASE_DIR, 'yolov3_testing.cfg')
+
+print(modelWeightsPath)
 
 # "C:\Users\julia\OneDrive\Área de Trabalho\TCC\COUNT\counteyeapi\services\detection\yolov3_training_last.weights"
 # "C:\Users\julia\OneDrive\Área de Trabalho\TCC\COUNT\counteyeapi\services\detection\yolov3_testing.cfg"
